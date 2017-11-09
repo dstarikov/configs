9cb57ff779f3e7a883728f015daa0d6563524e40
Daniel Starikov <starikov@uw.edu>
2017-11-09 11:24:30 -0800

    Adding fish stuff

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+fish_vi_mode
+
+export fish_color_host=yellow
+export fish_color_user=purple
+export fish_pager_color_progress=purple
+
+alias sl='ls --color'
+alias s='ls --color'
+alias ls='ls --color'
+alias rm='rm -I'
+alias cp='cp -i'
+alias wget='curl -O'
+alias h='history | tail -n 3'
+
+function x86
+  echo '(x86)'
+end
+function X86
+  echo '(X86)'
+end
