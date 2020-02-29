bdae8f665f6d2b37ea21f9e1f610d2bf905da0b0
Daniel Starikov <starikov@uw.edu>
2020-02-29 01:19:44 -0800

    fixing clipboard sync, powermenu, and xephyr/sommelier icon

--- .config/i3/start_i3.sh
+++ .config/i3/start_i3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-/usr/sbin/sommelier --accelerators="<Alt>tab,Super_L" -X --scale=1.00 Xephyr -name "i3_name" -title "i3" -br -ac -fullscreen -noreset -resizeable -glamor +iglx :3 &
+/usr/sbin/sommelier --glamor --shm-driver=virtwl-dmabuf --data-driver=virtwl --application-id="i3" --accelerators="<Alt>tab,Super_L" -X --scale=1.00 Xephyr -name "i3" -class "i3" -title "i3" -br -ac -fullscreen -noreset -resizeable -glamor +iglx :3 &
 unset DBUS_SESSION_BUS_ADDRESS && unset XDG_RUNTIME_DIR
-sleep 1	
 export TERMINAL=urxvt
+sleep 2
 DISPLAY=:3 i3
+killall onboard
 killall xclipsync
 killall xclipfrom
-killall onboard
 pkill Xephyr
